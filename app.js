d0e87034fbd3f3fd780e826b903d5337c0a29544
Alan García <ing.alangaro@gmail.com>
2022-11-05 00:11:16 -0600

    javascript OO - constructor

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('hello world')
+const user = {
+  name: "Alan",
+  lastName: "Garcia",
+  age: 30,
+  showFullName() {
+    return this.name + " " + this.lastName;
+  }
+}
+
+console.log(user.name)
+console.log(user.showFullName())
+
+// Constructor
+
+function Person() {
+  this.name2 = ""
+  this.lastName2 = ""
+  this.age2 = 0
+  this.showFullName2 = function () {
+    return `${this.name2} ${this.lastName2}`
+  }
+}
+
+const user2 = new Person()
+user2.name2 = "Alan"
+user2.lastName2 = "Garcia"
+user2.age2 = 30
+console.log(user2.showFullName2())
+
+const user3 = new Person()
+user3.name2 = "Jorge"
+user3.lastName2 = "Ruiz"
+user3.age2 = 34
+console.log(user3.showFullName2())
